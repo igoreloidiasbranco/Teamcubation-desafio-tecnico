1b78a156f8b848577a5f9e93f44616d8a6ddd70c
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-06-08 19:06:46 -0300

    Subindo projeto

---
+++ desafio/src/test/java/com/tecnico/desafio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecnico.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
