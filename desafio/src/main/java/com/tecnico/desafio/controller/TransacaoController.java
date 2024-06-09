a37fa131fc7fcd0b0f5b14762f30887752837f11
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-06-09 11:50:35 -0300

    Criação do método DELETE

--- desafio/src/main/java/com/tecnico/desafio/controller/TransacaoController.java
+++ desafio/src/main/java/com/tecnico/desafio/controller/TransacaoController.java
@@ -3,6 +3,7 @@ package com.tecnico.desafio.controller;
 import com.tecnico.desafio.dto.TransacaoDTO;
 import com.tecnico.desafio.service.TransacaoService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,12 @@ public class TransacaoController {
     public ResponseEntity salvar (@RequestBody TransacaoDTO transacaoDTO) {
         ResponseEntity responseEntity = transacaoService.salvar(transacaoDTO);
         return responseEntity;
+    }
+
+    @DeleteMapping("/transacao")
+    public ResponseEntity deletar () {
+        transacaoService.deletar();
+        return ResponseEntity.ok().build();
     }
 
 }
