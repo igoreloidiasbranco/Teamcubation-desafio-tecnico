1b3004279fd4d82a974ebcd3a0c3691e76ce0718
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-06-09 22:05:37 -0300

    Criação do método GET

--- desafio/src/main/java/com/tecnico/desafio/controller/TransacaoController.java
+++ desafio/src/main/java/com/tecnico/desafio/controller/TransacaoController.java
@@ -1,15 +1,13 @@
 package com.tecnico.desafio.controller;
 
+import com.tecnico.desafio.dto.EstatisticaDTO;
 import com.tecnico.desafio.dto.TransacaoDTO;
 import com.tecnico.desafio.service.TransacaoService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
-
 public class TransacaoController {
 
     final TransacaoService transacaoService;
@@ -29,6 +27,11 @@ public class TransacaoController {
     public ResponseEntity deletar () {
         transacaoService.deletar();
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/estatistica")
+    public ResponseEntity<EstatisticaDTO>  getEstatisticaDoUltimoMinuto() {
+        return transacaoService.getEstatisticaDoUltimoMinuto();
     }
 
 }
