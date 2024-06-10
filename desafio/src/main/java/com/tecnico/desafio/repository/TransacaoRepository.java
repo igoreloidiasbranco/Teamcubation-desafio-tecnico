d7721f56916315a141d5af57720be70b255bd101
Igor Eloi Dias Branco <igoreloi1981@gmail.com>
2024-06-09 22:14:54 -0300

    Criado retorno da lista de transação do último minuto

--- desafio/src/main/java/com/tecnico/desafio/repository/TransacaoRepository.java
+++ desafio/src/main/java/com/tecnico/desafio/repository/TransacaoRepository.java
@@ -3,6 +3,7 @@ package com.tecnico.desafio.repository;
 import com.tecnico.desafio.dto.TransacaoDTO;
 import org.springframework.stereotype.Repository;
 
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,5 +19,19 @@ public class TransacaoRepository {
 
     public void deletar() {
         this.transacaoDTOList.clear();
+    }
+
+    public List<TransacaoDTO> getTransacaoDTOListDoUltimoMinuto() {
+        List<TransacaoDTO> transacaoDTOListDoUltimoMinuto = new ArrayList<>();
+        OffsetDateTime agora = OffsetDateTime.now();
+        OffsetDateTime ultimoMinuto = agora.minusMinutes(1);
+
+        for (TransacaoDTO transacaoDTO : transacaoDTOList) {
+            OffsetDateTime dataHora = transacaoDTO.getDataHora();
+            if (dataHora.isAfter(ultimoMinuto) && dataHora.isBefore(agora)) {
+                transacaoDTOListDoUltimoMinuto.add(transacaoDTO);
+            }
+        }
+        return transacaoDTOListDoUltimoMinuto;
     }
 }
